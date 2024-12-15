a0162ecfae8f0c8910508f4400c75177551e592f
Mohit Uniyal <mohituniyal46@gmail.com>
2024-12-15 18:53:06 +0530

    fixed width

--- src/components/MainSidebar.tsx
+++ src/components/MainSidebar.tsx
@@ -50,7 +50,7 @@ export function SidebarDemo({children}: SidebarDemoProps) {
   return (
     <div
       className={cn(
-        "flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-7xl mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+        "flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
         "h-screen" // for your use case, use `h-screen` instead of `h-[60vh]`
       )}
     >
